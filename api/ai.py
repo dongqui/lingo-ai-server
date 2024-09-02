b59cd74a4f6e9c65a245b72c9752f385dc794073
Dongjin <dongqui9@gmail.com>
2024-09-02 18:58:11 +0900

    feat: model 수정

--- api/ai.py
+++ api/ai.py
@@ -1,5 +1,6 @@
 import os
-from langchain import OpenAI, LLMChain, PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 
@@ -7,16 +8,22 @@ load_dotenv()
 
 api_key = os.getenv('OPENAI_API_KEY')
 
-llm = OpenAI(api_key=api_key, temperature=0.7)
-
-template = ( "Provide only the feedback and improved sentence in {language} in the following format, with no extra text or labels: [Your feedback here]-[Improved sentence here, if needed] Sentence: {sentence}"
+llm = ChatOpenAI(
+    temperature=0.0,  # 창의성 (0.0 ~ 2.0)
+    model_name="gpt-4o",  # 모델명
 )
-prompt = PromptTemplate(template=template, input_variables=["feedback_language", "sentence"])
 
-chain = LLMChain(llm=llm, prompt=prompt)
+template = ( "Provide only the feedback and improved sentence in {language} in the following format, with no extra text or labels: [Your feedback here]--[Improved sentence here, if needed] Sentence: {sentence}"
+)
+prompt  = PromptTemplate.from_template(template)
 
-def get_response(sentence, feedback_language):
-    response = chain.run(sentence=sentence, feedback_language=feedback_language)
+chain = prompt | llm
+
+def get_response(sentence, language):
+    response = chain.invoke({
+        "sentence": sentence,
+        "language": language
+    })
     return response
 
-print(get_response('hello world, do you hear me?', 'Korean'))
+print(get_response(sentence='hello world, do you hear me?', language='Korean').content)
