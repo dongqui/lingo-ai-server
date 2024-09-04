bf2e0a9334337f1fc302408944dbe403d23e4809
Dongjin <dongqui9@gmail.com>
2024-09-04 16:35:22 +0900

    feat: POST method로 변경

--- index.py
+++ index.py
@@ -5,15 +5,23 @@ from ai import get_ai_response
 
 class Handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        sentence = "What is the capital of France?"
-        language = 'korean'
+    def do_POST(self):
+        # 요청 크기 가져오기
+        content_length = int(self.headers['Content-Length'])
+        
+        # 요청 본문에서 데이터 읽기
+        post_data = self.rfile.read(content_length)
+        
+        # JSON 형식의 데이터를 파싱
+        data = json.loads(post_data)
+        sentence = data.get('sentence')
+        language = data.get('language')
   
         response = get_ai_response(sentence, language)
         response_json = json.dumps(response)
       
         self.send_response(200)
-        self.send_header('Content-type', 'application/json;')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
 
         self.wfile.write(response_json.encode())
